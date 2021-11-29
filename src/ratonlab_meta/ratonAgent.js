dd30cb6fe44ff04d67e03cd63829f54bc2263872
JUAN PABLO SALGADO SALAS <juan.pablo.salgado@correounivalle.edu.co>
2021-11-28 22:21:40 -0500

    meta

--- src/ratonlab_meta/ratonAgent.js
+++ src/ratonlab_meta/ratonAgent.js
@@ -9,29 +9,95 @@ class CleanerAgent extends Agent {
         super(value);
         //LEFT, UP, RIGHT, DOWN, CELL
         this.table = {
-            "0,0,0,0,0": "UP",
-            "0,0,0,1,0": "UP",
-            "0,0,1,0,0": "UP",
-            "0,0,1,1,0": "LEFT",
-            "0,1,0,0,0": "LEFT",
-            "0,1,0,1,0": "RIGHT",
-            "0,1,1,0,0": "LEFT",
-            "0,1,1,1,0": "LEFT",
-            "1,0,0,0,0": "UP",
-            "1,0,0,1,0": "RIGHT",
-            "1,0,1,0,0": "DOWN",
-            "1,0,1,1,0": "UP",
-            "1,1,0,0,0": "RIGHT",
-            "1,1,0,1,0": "RIGHT",
-            "1,1,1,0,0": "DOWN",
-            "default": "TAKE"
+            "0,0,0,0,0": ["LEFT", "UP", "RIGHT", "DOWN"],
+            "0,0,0,1,0": ["LEFT", "UP", "RIGHT"],
+            "0,0,1,0,0": ["LEFT", "UP", "DOWN"],
+            "0,0,1,1,0": ["LEFT", "UP"],
+            "0,1,0,0,0": ["LEFT", "RIGHT", "DOWN"],
+            "0,1,0,1,0": ["LEFT", "RIGHT"],
+            "0,1,1,0,0": ["LEFT", "DOWN"],
+            "0,1,1,1,0": ["LEFT"],
+            "1,0,0,0,0": ["UP", "RIGHT", "DOWN"],
+            "1,0,0,1,0": ["UP","RIGHT"],
+            "1,0,1,0,0": [ "UP", "DOWN"],
+            "1,0,1,1,0": ["UP"],
+            "1,1,0,0,0": ["RIGHT", "DOWN"],
+            "1,1,0,1,0": ["RIGHT"],
+            "1,1,1,0,0": ["DOWN"],
+            "default": ["TAKE"]
         };
+
+    }
+    
+    setup(initialState = {}) {
+        this.initialState = initialState
+        
+        this.state=initialState
+        // this.state.data=JSON.parse(JSON.stringify(initialState.data))
+    }
+    updatex(position){
+        
+        // this.state.data[this.state.y][this.state.x]+=1
+        // console.log(this.state.data)
+        this.state.x=position.x
+        this.state.y=position.y
+
     }
 
-    setup(state0) {
-        this.x = state0.raton.x;
-        this.y = state0.raton.y;
-        this.queso0 = state0.queso;
+    setAction(){
+        let viewKey = this.perception.slice(0,5).join();
+        let possibleActions= this.table[viewKey];
+        console.log(possibleActions)
+        if(!possibleActions){
+            possibleActions = this.table['default']
+        }
+        let menor={
+            distance:Number.POSITIVE_INFINITY,
+            x:0,
+            y:0,
+            action:""
+        }
+        for(let i=0; i<possibleActions.length;i++){
+            let x=this.state.x
+            let y=this.state.y
+            switch (possibleActions[i]){
+                case 'UP':
+                    y-=1
+                    break
+                case 'DOWN':
+                    y+=1
+                    break
+                case 'RIGHT':
+                    x+=1
+                    break
+                case 'LEFT':
+                    x-=1
+                    break   
+        
+            }
+            if(x<0)x=0
+            
+            if(y<0)y=0
+            let chebyDist=Math.max(Math.abs(this.state.queso.x-x),Math.abs(this.state.queso.y-y))
+            console.log(chebyDist)
+            if(menor.distance>=chebyDist){
+                
+                menor.distance=chebyDist
+                menor.x=x
+                menor.y=y
+                menor.action=possibleActions[i]
+                
+            }
+        }
+            
+            if(this.state.data[menor.y][menor.x] !== 1){
+                
+                return [menor.action,{x:menor.x,y:menor.y}]
+            }
+
+        
+        
+        
     }
 
     /**
@@ -39,22 +105,17 @@ class CleanerAgent extends Agent {
      * In this case, the state is just obtained as the join of the perceptions
      */
     send() {
-        //this.perception = [LEFT, UP, DOWN, RIGTH, SMELL, ratonx, ratony, qx, qy]
-
         
-        let viewKey = this.perception.join();
-        //let action = foo(this.internalState, this.perception)
-        //this.internalState = updatex(this.internalState, this.perception, action)
-        //return action;
+        let [action,position] = this.setAction()
+      
+        this.updatex(position)
 
-        if (this.table[viewKey]) {
-            return this.table[viewKey];
-        } else {
-            return this.table["default"];
-        }
+        return action;
 
     }
+    
 
 }
+
 
 module.exports = CleanerAgent;
