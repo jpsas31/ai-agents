92d4667e30604d1edb63e3b4e55ae5f214a39268
Santiago Rojas <santiago.barreneche@correounivalle.edu.co>
2022-01-10 14:00:40 -0500

    Se añade constructor con parametros a posicion

--- posicion.py
+++ posicion.py
@@ -2,3 +2,10 @@ class Posicion:
     def __init__(self):
         self.x = -1
         self.y = -1
+    
+    @classmethod
+    def positionGiven(cls, x, y):
+        aux = cls()
+        aux.x = x
+        aux.y = y
+        return aux
