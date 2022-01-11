4fab5275c1a8294ca248ccacda8c09725ffc390f
Santiago Rojas <santiago.barreneche@correounivalle.edu.co>
2022-01-10 20:00:42 -0500

    Se cambia el nombre de las variables para que cumplan con el estandar de python y se añade el diccionario de mundo para optimizar el rendimiento

--- nodo.py
+++ nodo.py
@@ -6,85 +6,78 @@ class Nodo:
         self.estado = Estado()
         self.padre = None
         self.solucion = ''
-        self.operadorAplicado = ''
         self.profundidad = 0
-        self.moviCaja = False
-        self.posPuntosLLegada = []
+        self.mundo = {}
         
-    def esMeta(self):
-        for punto in self.posPuntosLLegada:
-            for index,caja in enumerate(self.estado.posCajas):
-                if(punto.x == caja.x and punto.y == caja.y):
-                    break
-                if(index == len(self.estado.posCajas)-1):
-                    return False
+    def es_meta(self):
+        for caja in self.estado.pos_cajas:
+            if(self.estado.tablero[caja.x][caja.y] != 'X'):
+                return False
         return True
 
     #metodos que facilitan la creacion de nodos de acuerdo a las reglas del juego
 
     #retorna verdadero o falso si sobre la posicion pasada se encuentra una caja
-    def hayCaja(self,posicion):
-        hay_Caja = False
-        for caja in self.estado.posCajas:
+    def hay_caja(self,posicion):
+        for caja in self.estado.pos_cajas:
             if(posicion.x == caja.x and posicion.y == caja.y):
-                hay_Caja = True
-        return hay_Caja
+                return True
+        return False
     
-    def hayMuro(self,posicion):
+    def hay_muro(self,posicion):
         return self.estado.tablero[posicion.x][posicion.y] == 'W'
 
     #actualiza la posicion de una caja para un nuevo nodo retornando la lista de cajas (por valor)
-    def actualizarCaja(self, posicionActual, nuevaPosicion):
-        cajas = self.estado.posCajas[:]
-        nuevasCajas = []
+    def actualizar_caja(self, posicion_actual = False, nueva_posicion = False):
+        cajas = self.estado.pos_cajas[:]
+        nuevas_cajas = []
         for caja in cajas:
-            if(caja.x == posicionActual.x and caja.y == posicionActual.y):
-                aux = Posicion.positionGiven(nuevaPosicion.x, nuevaPosicion.y)
-                nuevasCajas.append(aux)
+            if(caja.x == posicion_actual.x and caja.y == posicion_actual.y):
+                aux = Posicion.position_given(nueva_posicion.x, nueva_posicion.y)
+                nuevas_cajas.append(aux)
             else:
-                nuevasCajas.append(caja)
-        return nuevasCajas
+                nuevas_cajas.append(caja)
+        return nuevas_cajas
     
-    def crearNodo(self,posicionAlmacenista,operador,moviCaja = False,posicionCaja = False):
-
-        nuevaPosicion = posicionAlmacenista
-
-        estado = Estado()
-        estado.tablero = self.estado.tablero
-        estado.posAlmacenista = nuevaPosicion 
-        if(posicionCaja != False):
-            estado.posCajas = self.actualizarCaja(posicionAlmacenista,posicionCaja)
-        else:
-            estado.posCajas = self.estado.posCajas[:]
-
+    def crear_nodo(self,posicion_almacenista,operador,posicion_caja = False):
         nodo = Nodo()
-        nodo.estado = estado
-        nodo.solucion = self.solucion + operador
-        nodo.moviCaja = moviCaja
-        nodo.profundidad = self.profundidad + 1
-        nodo.padre = self
-        nodo.operadorAplicado = operador
-        nodo.posPuntosLLegada = self.posPuntosLLegada
+        estado = Estado()
 
+        estado.tablero = self.estado.tablero
+        estado.pos_almacenista = posicion_almacenista
+
+        if(posicion_caja != False):
+            estado.pos_cajas = self.actualizar_caja(posicion_almacenista,posicion_caja)
+        else:
+            estado.pos_cajas = self.estado.pos_cajas[:]
+            nodo.mundo = self.mundo
+
+        nodo.estado = estado
+        nodo.padre = self
+        nodo.solucion = self.solucion + operador
+        nodo.profundidad = self.profundidad + 1
         return nodo
 
-    def crearNodoOperador(self,nuevaPosicion,operador, nuevaPosicionCaja):
-        if(not(self.hayMuro(nuevaPosicion))):
-            if(self.hayCaja(nuevaPosicion)):
-                if(not(self.hayMuro(nuevaPosicionCaja)) and not(self.hayCaja(nuevaPosicionCaja))):
-                    return self.crearNodo(nuevaPosicion,operador,True,nuevaPosicionCaja)
+    def puedo_crear_nodo(self,nueva_posicion,operador, nueva_posicion_caja):
+        if(self.mundo.get(str(nueva_posicion)) == True):
+            return None
+        if(not(self.hay_muro(nueva_posicion))):
+            if(self.hay_caja(nueva_posicion)):
+                if(not(self.hay_muro(nueva_posicion_caja)) and not(self.hay_caja(nueva_posicion_caja))):
+                    return self.crear_nodo(nueva_posicion,operador,nueva_posicion_caja)
             else:
-                return (self.crearNodo(nuevaPosicion,operador))
+                return (self.crear_nodo(nueva_posicion,operador))
         return None
 
-    def crearNodos(self):
+    def crear_nodos(self):
         nodos = []
-        pos = self.estado.posAlmacenista
-    
-        nodos.append(self.crearNodoOperador(Posicion.positionGiven(pos.x-1,pos.y),'U',Posicion.positionGiven(pos.x-1-1,pos.y)))
-        nodos.append(self.crearNodoOperador(Posicion.positionGiven(pos.x+1,pos.y),'D',Posicion.positionGiven(pos.x+1+1,pos.y)))
-        nodos.append(self.crearNodoOperador(Posicion.positionGiven(pos.x,pos.y-1),'L',Posicion.positionGiven(pos.x,pos.y-1-1)))   
-        nodos.append(self.crearNodoOperador(Posicion.positionGiven(pos.x,pos.y+1),'R',Posicion.positionGiven(pos.x,pos.y+1+1)))
+        pos = self.estado.pos_almacenista    
+        self.mundo[str(self.estado.pos_almacenista)] = True
+
+        nodos.append(self.puedo_crear_nodo(Posicion.position_given(pos.x-1,pos.y),'U',Posicion.position_given(pos.x-1-1,pos.y)))
+        nodos.append(self.puedo_crear_nodo(Posicion.position_given(pos.x+1,pos.y),'D',Posicion.position_given(pos.x+1+1,pos.y)))
+        nodos.append(self.puedo_crear_nodo(Posicion.position_given(pos.x,pos.y-1),'L',Posicion.position_given(pos.x,pos.y-1-1)))   
+        nodos.append(self.puedo_crear_nodo(Posicion.position_given(pos.x,pos.y+1),'R',Posicion.position_given(pos.x,pos.y+1+1)))
 
         aux = []
         for nodo in nodos:
