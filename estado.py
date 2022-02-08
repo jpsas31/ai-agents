dbdd1f082be7886ba4a5c90e96b7c07e746a8eca
Santiago Rojas <santiago.barreneche@correounivalle.edu.co>
2022-02-08 11:17:02 -0500

    Se añade el metodo que permite comparar dos objetos de tipo estado

--- estado.py
+++ estado.py
@@ -4,4 +4,19 @@ class Estado:
     def __init__(self):
         self.pos_almacenista = Posicion()  
         self.pos_cajas = []
+
+    def __eq__(self, other):
+        if(type(other) != type(self)):
+            return False
         
+        if(len(self.pos_cajas) != len(other.pos_cajas)):
+            return False
+
+        valor = True
+
+        for index,posCaja in enumerate(self.pos_cajas):
+            if(posCaja != other.pos_cajas[index]):
+                valor = False
+                break
+
+        return ((self.pos_almacenista == other.pos_almacenista) and valor )
